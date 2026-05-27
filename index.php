f449aa2c8bf102176563982c6965598cfa2a432f
zhelonkinavictoria20077-spec <zhelonkinavictoria20077@gmail.com>
2026-05-28 01:06:32 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,76 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>МФЦ — Главная</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<header>
+    <div class="container header">
+        <div class="logo">
+            Система организации записи на посещение МФЦ
+        </div>
+
+        <div class="nav">
+            <a href="login.php" class="login">Авторизация</a>
+            <a href="register.php" class="register">Регистрация</a>
+        </div>
+    </div>
+</header>
+
+<main>
+
+<!-- HERO БЛОК -->
+<section class="hero">
+    <div class="container hero-content">
+
+        <div class="hero-text">
+            <p>
+                Планируйте визит в МФЦ заранее и экономьте время.<br>
+                Выбирайте удобную дату и время, чтобы получить услугу без очередей.<br>
+            </p>
+        </div>
+
+        <div class="hero-image">
+            <img src="img/глав.jpg" alt="Иллюстрация записи на посещение">
+        </div>
+
+    </div>
+</section>
+
+<!-- БЛОК ПРЕИМУЩЕСТВ -->
+<section class="features">
+    <div class="container features-container">
+
+        <div class="feature-box">
+            <img src="img/ик 1.png" alt="Иконка календаря">
+            <h3>Простая запись</h3>
+            <p>Выберите нужную услугу и время визита всего за пару кликов.</p>
+        </div>
+
+        <div class="feature-box">
+            <img src="img/ик2.png" alt="Иконка статуса">
+            <h3>Удобный контроль</h3>
+            <p>Отслеживайте изменения в вашей записи: подтверждена, отменена, завершена.</p>
+        </div>
+
+    </div>
+</section>
+
+</main>
+
+<footer>
+    <div class="container footer">
+        Контакты: р.п. Шемышейка, ул. Ленина, д.10 <br>
+        Телефон: +7 (8412) 123-45-20 <br>
+        Пн-Пт 8:00–18:00 <br>
+        Сб 8:00-13:00
+    </div>
+</footer>
+
+</body>
+</html>
